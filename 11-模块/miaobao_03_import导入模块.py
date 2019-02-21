71185f3ddfef549cf82703834dad0e4a3359d97d
miaobaobao <miaomiaobabyzy@gmail.com>
2019-02-21 18:51:13 +0800

    Create miaobao_03_import导入模块.py

---
+++ 11-模块/miaobao_03_import导入模块.py
@@ -0,0 +1,11 @@
+import miaobao_01_测试模块1
+import miaobao_02_测试模块2
+
+miaobao_01_测试模块1.say_hello()
+miaobao_02_测试模块2.say_hello()
+
+dag = miaobao_01_测试模块1.Dog()
+print(dag)
+
+cat = miaobao_02_测试模块2.Cat()
+print(cat)
