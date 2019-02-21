63bce5c9fc6afaa3e174bcb8f7c324f8809e0744
miaobaobao <miaomiaobabyzy@gmail.com>
2019-02-21 18:43:32 +0800

    Create miaobao_07_多个返回值.py

---
+++ 07-语法进阶/miaobao_07_多个返回值.py
@@ -0,0 +1,28 @@
+def measure():
+    """测量湿度和温度"""
+    print("测量开始...")
+    temp = 39
+    wetness = 50
+    print("测量结束")
+
+    # 元素 - 可以包含多个数据，因此可以让函数一次返回多个值
+    # 如果函数返回的类型是元组， 小括号可以省去
+    # return (temp, wetness)
+    return temp, wetness
+
+
+# result 保存的是元组类型
+result = measure()
+print(result)
+
+# 需要单独的处理温度或者湿度 - 不方便
+print(result[0])
+print(result[1])
+
+
+# 如果函数返回的类型是元组，同时希望单独的处理元组中的元素
+# 可以使用多个变量，一次接受函数的返回结果
+# 注意： 使用多个变量接受结果时，变量的个数需要和元组的个数保持一致
+gl_temp, gl_wetness = measure()
+print(gl_temp)
+print(gl_wetness)
