36b50eb0134245d88afd08670a38208e265ceccd
miaobaobao <miaomiaobabyzy@gmail.com>
2019-02-21 17:24:42 +0800

    Create miaobao_09_字典的定义.py

---
+++ 05-高级数据类型/miaobao_09_字典的定义.py
@@ -0,0 +1,9 @@
+# 字典是一个无序的数据集合
+# 输出的顺序和定义的顺序是不一样的
+student = {"name": "小明",
+            "age": 18,
+            "gender": True,
+            "height": 1.78,
+            "weight": 60}
+
+print(student)
