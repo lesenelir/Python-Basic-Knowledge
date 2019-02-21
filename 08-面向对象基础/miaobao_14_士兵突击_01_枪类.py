505609608334315bc3c88593dfba49b66cab7d03
miaobaobao <miaomiaobabyzy@gmail.com>
2019-02-21 18:45:42 +0800

    Create miaobao_14_士兵突击_01_枪类.py

---
+++ 08-面向对象基础/miaobao_14_士兵突击_01_枪类.py
@@ -0,0 +1,25 @@
+class Gun:
+    def __init__(self, model):
+        # 枪的型号
+        self.model = model
+        # 子弹数量
+        self.bullet_count = 0
+
+    def add_bullet(self, count):
+        self.bullet_count += count
+
+    def shoot(self):
+        # 判断子弹数量
+        if self.bullet_count <= 0:
+            print("[%s] 没有子弹了..." % self.model)
+        # 发射子弹 子弹数量-1
+        self.bullet_count -= 1
+        # 提示发射信息
+        print("[%s] 突突突...[%d]" % (self.model, self.bullet_count))
+
+
+# 创建枪对象
+ak47 = Gun("AK47")
+
+ak47.add_bullet(50)
+ak47.shoot()
