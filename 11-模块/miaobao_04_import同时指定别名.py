0f8a33d939f9a51f79fc17cfd944eaca87c417ba
miaobaobao <miaomiaobabyzy@gmail.com>
2019-02-21 18:51:15 +0800

    Create miaobao_04_import同时指定别名.py

---
+++ 11-模块/miaobao_04_import同时指定别名.py
@@ -0,0 +1,11 @@
+import miaobao_01_测试模块1 as DogModule
+import miaobao_02_测试模块2 as CatModule
+
+DogModule.say_hello()
+CatModule.say_hello()
+
+dog = DogModule.Dog()
+print(dog)
+
+cat = CatModule.Cat()
+print(cat)
