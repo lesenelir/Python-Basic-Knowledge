69bf6334a859eaccfd0f15098e5573341b83ee36
miaobaobao <miaomiaobabyzy@gmail.com>
2019-02-21 17:23:06 +0800

    Create miaobao_01_九九乘法表.py

---
+++ 04-函数/miaobao_01_九九乘法表.py
@@ -0,0 +1,12 @@
+def multiple_table():
+    row = 1
+    while row <= 9:
+
+        col = 1
+        while col <= row:
+            # print("*", end="")
+            print("%d * %d = %d" % (col, row, row * col), end="\t")
+            col = col + 1
+        print("")
+        # print("%d" % row)
+        row = row + 1
