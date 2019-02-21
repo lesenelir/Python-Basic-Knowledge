d426de8d2dc7ae68426b0d92de7f5fcb9ec6e740
miaobaobao <miaomiaobabyzy@gmail.com>
2019-02-21 17:21:23 +0800

    Create miaobao_04_节日.py

---
+++ 02-分支/miaobao_04_节日.py
@@ -0,0 +1,10 @@
+holiday_name = "情人节"
+if holiday_name == "情人节":
+    print("买玫瑰")
+    print("看电影")
+elif holiday_name == "平安夜": #elif后面要加条件
+    print("买苹果")
+elif holiday_name == "生日":
+    print("买蛋糕")
+else:
+    print("每天都是节日")
