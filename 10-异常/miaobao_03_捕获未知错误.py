2d98e69ff20bbc7ffde53d2498c5e5cf822904ee
miaobaobao <miaomiaobabyzy@gmail.com>
2019-02-21 18:48:11 +0800

    Create miaobao_03_捕获未知错误.py

---
+++ 10-异常/miaobao_03_捕获未知错误.py
@@ -0,0 +1,12 @@
+try:
+    # 提示用户输入一个整数
+    num = int(input("输入一个整数："))
+    # 使用8除以用书输入的整数并且输出
+    result = 8 / num
+    print(result)
+# python解释器抛出异常时，最后一行错误信息的第一个单词，就是错误类型
+
+except ValueError:
+    print("请输入正确的整数")
+except Exception as result_except:
+    print("未知错误 %s" % result_except)
