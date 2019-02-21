0a7f27f877e4b9f3a6a0d9f698474f2aa5c45db5
miaobaobao <miaomiaobabyzy@gmail.com>
2019-02-21 18:47:27 +0800

    Create miaobao_18_单例初始化一次.py

---
+++ 09-面向对象特性/miaobao_18_单例初始化一次.py
@@ -0,0 +1,32 @@
+class MusicPlayer(object):
+    # 定义类属性，记录第一个被创建对象的引用
+    instance = None
+
+    # 记录是否执行过初始化方法
+    init_flag = False
+
+    def __new__(cls, *args, **kwargs):
+        # 判断类属性是否是空对象
+        if cls.instance is None:
+            # 调用父类的方法，为第一个对象分配空间
+            cls.instance = super().__new__(cls)
+        # 返回类属性保存的对象引用
+        return cls.instance
+
+    def __init__(self):
+        # 判断类属性，是否执行过初始化动作
+        if MusicPlayer.init_flag:  # 真
+            return
+        # 如果没有执行过，再执行初始化动作
+        print("初始化播放器")
+        # 修改类属性的标记
+        MusicPlayer.init_flag = True
+
+
+# 创建多个对象
+player1 = MusicPlayer()
+print(player1)
+
+player2 = MusicPlayer()
+print(player2)
+
